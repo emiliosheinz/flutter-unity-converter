15ebcdda5f8bb3ae524bdd10d6331c7d89771e73
emilioheinz <emiliosheinz@gmail.com>
2020-02-12 21:44:56 -0300

    Moving to funtions the diferent components

--- unity_converter/lib/pages/calculate.dart
+++ unity_converter/lib/pages/calculate.dart
@@ -1,6 +1,75 @@
 import 'package:flutter/material.dart';
 
 class CalculatePage extends StatelessWidget {
+  _renderInputFields() {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Input',
+            labelStyle: TextStyle(color: Colors.black),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.black,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ),
+        DropdownButtonFormField()
+      ],
+    );
+  }
+
+  _renderOutputFields() {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Output',
+            labelStyle: TextStyle(color: Colors.black),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.black,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ),
+        DropdownButtonFormField(),
+      ],
+    );
+  }
+
+  _renderForm() {
+    return Form(
+      child: Column(
+        children: <Widget>[
+          _renderInputFields(),
+          SizedBox(
+            height: 20,
+          ),
+          Icon(
+            Icons.compare_arrows,
+            size: 50,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          _renderOutputFields(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,31 +89,8 @@ class CalculatePage extends StatelessWidget {
                   child: Image.asset('assets/icons/area.png'),
                 ),
               ),
-              SizedBox(height: 20),
-              Form(
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(hintText: 'Input'),
-                    ),
-                    DropdownButtonFormField(),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Icon(
-                      Icons.compare_arrows,
-                      size: 50,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(hintText: 'Output'),
-                    ),
-                    DropdownButtonFormField(),
-                  ],
-                ),
-              )
+              SizedBox(height: 50),
+              _renderForm(),
             ],
           ),
         ),
